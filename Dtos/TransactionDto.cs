aa2b3f21ff5d54ad6278600bf5620109bf0df6b4
HungPham <hungpham.tns@gmail.com>
2025-07-28 01:28:40 +0700

    API Transaction

---
+++ Dtos/TransactionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BankingSystem.Dtos
+{
+    public class TransactionDto
+    {
+       
+            public DateTime TransactionDate { get; set; }
+            public string FromAccount { get; set; }
+            public string ToAccount { get; set; }
+            public decimal Amount { get; set; }
+            public string Content { get; set; }
+            public string TransactionType { get; set; } // Internal hoặc External
+        
+
+    }
+}
